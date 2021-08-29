743c329bcf78b503f0b0a5309dc0036844cc7774
gonzoden <deniz.nazarova.99@gmail.com>
2021-08-29 23:45:08 +0600

    modified R code

--- main.R
+++ main.R
@@ -9,10 +9,34 @@ response <- read.csv("~/.../mentalh_ugrad.csv", header = T)
 #length(response) #number of variables (questions)
 #names(response) #names of the vars
 
-#loaded only real responders who completed the survey
-df <- subset (response, Status == "IP Address" & Progress == 100, select = c("A3", "A4")) 
+#load only real responders who completed the survey
+#df <- subset (response, Status == "IP Address" & Progress == 100, select = c("A3", "A4")) 
+df <- subset (response, Status == "IP Address" & Progress == 100) 
 
-data(df)
-mosaicplot(df)
-#print(df)
+##df's index = /csv index - 1 #'cause count starts from 0
+
+#load only those of df who did not report on troubles with mental health
+mn <- subset(df, B1 == "No", select = c("B1"))
+print(mn)
+
+#load only those of df who reported on troubles with mental health
+mi <- subset(df, B1 != "No", select = c("B1"))
+print(mi)
+
+#create comparison to those who reported "No" vs. !"No"
+#?????
+
+#create a piechart on symptoms distribution
+slices <- c(10, 12, 4, 16, 8)
+lbls <- c("Sleeping too much or too little", 
+          "Poor concentration",
+          "Feeling restless, irritable, or tense", 
+          "Thinking or moving slower than usual",
+          "Decrease of immune system")
+
+pct <- round(slices/sum(slices)*100)
+lbls <- paste(lbls, pct) # add percents to labels
+lbls <- paste(lbls,"%",sep="") # ad % to labels
+pie(slices,labels = lbls, col=rainbow(length(lbls)),
+    main="Spread of Depression among majors")
 
